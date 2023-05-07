d2874d6b9bf8218ebee8a2cd34c1f86dc0653fb4
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-05-07 12:40:29 +0200

    Allowing Map Interaction & Adding Markers

--- screens/Map.js
+++ screens/Map.js
@@ -1,15 +1,39 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 const Map = () => {
-  const initialRegion = {
+  const initialPosition = {
     latitude: 40.640063,
     longitude: 22.944419,
+  }
+  
+  const initialRegion = {
+    ...initialPosition,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
 
-  return <MapView initialRegion={initialRegion} style={styles.map}></MapView>;
+  const [selectedLocation, setSelectedLocation] = useState(
+    // initialPosition // Εάν θέλω να ξεκινήσει με πινέζα στον χάρτη σε συγκεκριμένο σημείο
+  );
+
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ latitude: lat, longitude: lng });
+  };
+
+  return (
+    <MapView
+      initialRegion={initialRegion}
+      style={styles.map}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && <Marker title='Επιλεγμένη Τοποθεσία' coordinate={selectedLocation}/>}
+    </MapView>
+  );
 };
 export default Map;
 
